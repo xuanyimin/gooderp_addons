f5f6a919b6408534fc4d4f66a855fd282bfaeb3b
jeffery CHEN fan <jeffery9@gmail.com>
2018-05-19 09:50:32 +0800

    增加 权限

--- nonprofit_money/__manifest__.py
+++ nonprofit_money/__manifest__.py
@@ -10,13 +10,14 @@
                             该模块实现了非营利组织的功能。
     ''',
     'version': '11.11',
-    'depends': ['keep_good','auto_exchange','tree_zero_display_blank'],
+    'depends': ['keep_good','auto_exchange','tree_zero_display_blank','money','core'],
     'data': [
         'views/inout_activities_view.xml',
         'views/nonprofit_money_order_view.xml',
         'security/groups_ccb.xml',
         'data/inout_activities_template.xml',
         'data/inout_export_template_data.xml',
+        'security/ir.model.access.csv',
     ],
     'demo': [],
     'application': True,
